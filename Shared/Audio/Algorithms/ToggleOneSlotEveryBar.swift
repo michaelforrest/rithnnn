fd235b18873d070bf688cf7eadffcf0f4c4cb870
Michael Forrest <michael.forrest@gmail.com>
2021-09-26 16:44:50 +0100

    Loosen up the algo

--- Shared/Audio/Algorithms/ToggleOneSlotEveryBar.swift
+++ Shared/Audio/Algorithms/ToggleOneSlotEveryBar.swift
@@ -12,7 +12,7 @@ class ToggleOneSlotEveryBar: ObservableObject, Algorithm{
     var updatesEvery: RecalculationFrequency = .bar
     
     @Published var currentRifff: Rifff?
-    @Published var changeCountdown = 4
+    @Published var changeCountdown = 32
     
     func start(document: RithnnnDocument, player: Player, at time: AVAudioTime) throws{
         let rifff = document.manifest.rifffs.first!
@@ -24,14 +24,14 @@ class ToggleOneSlotEveryBar: ObservableObject, Algorithm{
     func scheduleChanges(at time: AVAudioTime, on player: Player, document: RithnnnDocument) throws{
         changeCountdown -= 1
         if changeCountdown <= 0{
-            changeCountdown = 4 // [8,16,32].randomElement()!
+            changeCountdown = [8,16,32].randomElement()!
             if let rifff = document.manifest.rifffs.randomElement(){
                 DispatchQueue.main.async {
                     try? self.playAllLoops(rifff: rifff, player: player, at: time)
                 }
                 self.currentRifff = rifff
             }
-        }else{
+        }else if changeCountdown % 4 == 0{
             player.run(at: time) {
                 player.slots.filter({ $0.currentAudioLoop != nil }).randomElement()?.toggleMuted()
             }
